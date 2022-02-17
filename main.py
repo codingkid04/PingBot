c4ff9e727e6b21f334bed27bd61416ad54850cdd
codingkid04 <dmpope93@gmail.com>
2022-02-16 23:06:26 -0600

    Added Welcome DM

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('PingBot is ready to roll!')
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hey {member.name}, welcome to the **CAPSOC Discord Server!** Here you can find all kinds of stuff, but before you do anything, please take time to look into our **Rules** channel and get familiar with it. If you have any questions or concerns, please contact the moderators of this sever!'
+    )
 
 @client.command()
 async def ping(ctx):
