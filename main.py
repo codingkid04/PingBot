eaa78a33e5b972d8c7a03822d88270c3839aba97
codingkid04 <dmpope93@gmail.com>
2022-02-17 00:00:49 -0600

    Added Welcome DM Feature (Finally!)

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ import discord
 
 from dotenv import load_dotenv
 from discord.ext import commands
+intents=discord.Intents.all()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='.',intents=intents)
 
 @client.event
 async def on_ready():
     print('PingBot is ready to roll!')
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hey {member.name}, welcome to the **CAPSOC Discord Server!** Here you can find all kinds of stuff, but before you do anything, please take time to look into our **Rules** channel and get familiar with it. If you have any questions or concerns, please contact the moderators of this sever!'
-    )
 
 @client.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+
+@client.command()
+async def say(ctx, arg):
+	await ctx.channel.send(arg)
+
+@client.event
+async def on_member_join(member):
+    await member.send(f"Hey {member.name}, welcome to the **CAPSOC Discord Server!** Here you can find all kinds of stuff, but before you do anything, please take time to look into our **Rules** channel and get familiar with it. If you have any questions or concerns, please contact the moderators of this sever!")
+
 
 @client.command()
 async def message(ctx, user:discord.Member, *, message=None):
     message = "Welcome to the Server!"
     embed = discord.Embed(title=message)
     await user.send(embed=embed)
+
 
 client.run(TOKEN)
